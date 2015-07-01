79a9f1b05235668ce32a160bd273e956bb4e2031
zh-wang <viennakanon@gmail.com>
2015-07-01 18:25:25 +0900

    cathage support

--- SwiftySurrogate.podspec
+++ SwiftySurrogate.podspec
@@ -9,7 +9,7 @@
 
 Pod::Spec.new do |s|
   s.name             = "SwiftySurrogate"
-  s.version          = "0.1.3"
+  s.version          = "1.0.0"
   s.summary          = "Use UTF16 surrogate easier in Swift"
   s.homepage         = "https://github.com/zh-wang/SwiftySurrogate"
   s.license          = 'MIT'
---
+++ SwiftySurrogate/SwiftySurrogate/SwiftySurrogate.h
@@ -0,0 +1,19 @@
+//
+//  SwiftySurrogate.h
+//  SwiftySurrogate
+//
+//  Created by Wang Zhenghong on 2015/07/01.
+//  Copyright (c) 2015年 FAL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftySurrogate.
+FOUNDATION_EXPORT double SwiftySurrogateVersionNumber;
+
+//! Project version string for SwiftySurrogate.
+FOUNDATION_EXPORT const unsigned char SwiftySurrogateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftySurrogate/PublicHeader.h>
+
+
