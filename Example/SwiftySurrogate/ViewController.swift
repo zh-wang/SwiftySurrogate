e04b623982eb1f9aafd7b65e040cca4abb4e7827
zh-wang <viennakanon@gmail.com>
2015-09-28 16:12:52 +0900

    fix example

--- Example/SwiftySurrogate/ViewController.swift
+++ Example/SwiftySurrogate/ViewController.swift
@@ -15,23 +15,23 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        var label: UILabel = UILabel(frame: CGRectMake(0, self.view.frame.midY - 50, self.view.frame.width, 100))
+        let label: UILabel = UILabel(frame: CGRectMake(0, self.view.frame.midY - 50, self.view.frame.width, 100))
         label.text = "input: D83D:DCC9 | 0xD83C, 0xDF80 | 0x2702"
         self.view.addSubview(label)
         
-        var label2: UILabel = UILabel(frame: CGRectMake(0, self.view.frame.midY - 50 - 100, self.view.frame.width, 100))
-        var emoji1 = SwiftySurrogate.decodeFromSurrogatePair(surrogatePair: "D83D:DCC9")
-        var emoji2 = SwiftySurrogate.decodeFromSurrogatePair(high: 0xD83C, low: 0xDF80)
-        var emoji3 = String(UnicodeScalar(0x2702))
+        let label2: UILabel = UILabel(frame: CGRectMake(0, self.view.frame.midY - 50 - 100, self.view.frame.width, 100))
+        let emoji1 = SwiftySurrogate.decodeFromSurrogatePair(surrogatePair: "D83D:DCC9")
+        let emoji2 = SwiftySurrogate.decodeFromSurrogatePair(high: 0xD83C, low: 0xDF80)
+        let emoji3 = String(UnicodeScalar(0x2702))
         label2.text = "\(emoji1) | \(emoji2) | \(emoji3)"
         self.view.addSubview(label2)
         
         // This will fail
-        if let res = SwiftySurrogate.decodeFromSurrogatePair(surrogatePair: "FFFF:DE04") {
+        if let _ = SwiftySurrogate.decodeFromSurrogatePair(surrogatePair: "FFFF:DE04") {
             
         } else {
             // Get the error
-            println(SwiftySurrogate.lastError())
+            print(SwiftySurrogate.lastError())
         }
         
     }
